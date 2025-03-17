ce253c7a8accffcb5f98feebc955d949b969fddb
Jitin Kumar <jitin.kumar@xceedance.com>
2025-03-17 17:20:18 +0530

    Code pushed in Repo

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApi.Controllers
+{
+    [ApiController]
+    public class ErrorController : ControllerBase
+    {
+        [HttpGet("/error")]
+        public IActionResult Error()
+        {
+            IExceptionHandlerFeature context = HttpContext.Features.Get<IExceptionHandlerFeature>();
+
+            if (context is not null)
+            {
+                string stackTrace = context.Error.StackTrace;
+                string errorMessage = context.Error.Message;
+
+                // Log Problem 
+            }
+
+            return Problem();
+        }
+    }
+}
---
+++ Models/ApiConfiguration.cs
@@ -0,0 +1,13 @@
+namespace DemoApi.Models
+{
+    public class ApiConfiguration
+    {
+        public string ApiName { get; set; }
+
+        public string ApiDescription { get; set;}
+
+        public string ApiVersion { get; set; }
+
+        public string ApiBaseUrl { get; set; }
+    }
+}
